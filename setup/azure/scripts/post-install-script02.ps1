93e21feebcfd33aca9e33f012bd61f4f42f9b8dc
amal gireesh <48020356+amalgireesh@users.noreply.github.com>
2020-07-23 18:30:58 +0530

    Update post-install-script02.ps1

--- setup/azure/scripts/post-install-script02.ps1
+++ setup/azure/scripts/post-install-script02.ps1
@@ -1,1 +1,23 @@
+. C:\LabFiles\AzureCreds.ps1
+
+$userName = $AzureUserName
+$password = $AzurePassword
+
+$securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $userName, $SecurePassword
+
+Connect-AzAccount -Credential $cred | Out-Null
+
+$resourceGroupName = (Get-AzResourceGroup | Where-Object { $_.ResourceGroupName -like "*Synapse-AIAD-*" }).ResourceGroupName
+$deploymentId =  (Get-AzResourceGroup -Name $resourceGroupName).Tags["DeploymentId"]
+
+# Template deployment
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+  -TemplateUri "https://raw.githubusercontent.com/CloudLabs-Samples/Workshop-Setup/main/setup/azure/innertemplates/deploy-synapse-workspace.json" `
+  -deploymentId $deploymentId
+
+New-AzRoleAssignment -ResourceGroupName $resourceGroupName -ErrorAction Ignore -ObjectId "37548b2e-e5ab-4d2b-b0da-4d812f56c30e" -RoleDefinitionName "Owner"
+
+sleep 20
+Unregister-ScheduledTask -TaskName "Setup" -Confirm:$false
 
